372c4f879aa85ad7789c62c9c2e9974270121711
Soliyana Yalewdeg <soliyanayalew22@gmail.com>
2025-07-22 08:45:13 +0300

    commit

--- src/Components/Navigation/Navigation_list.jsx
+++ src/Components/Navigation/Navigation_list.jsx
@@ -1,35 +1,50 @@
-import React from 'react'
-import { SearchIcon,Heart, ShoppingCart } from 'lucide-react'
+import React, { useState } from 'react'
+import { SearchIcon, ShoppingCart } from 'lucide-react'
 import { FiHeart } from 'react-icons/fi'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { Menu, X } from 'lucide-react'
 import './Navigation.css'
+
 const Navigation_list = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <div className="Navigation-container">
+      {/* Logo/Brand */}
       <div className="exclusive_text">
         <p>Exclusive</p>
       </div>
-      <div className='navigation'>
-        <nav className='navigation-lists'>
-        
-            <Link  to="/" className='navigation-page'>Home</Link>
-            <Link  to="/contact" className='navigation-page'>Contact</Link>
-            <Link  to="/about" className='navigation-page'>About</Link>
-            <Link  to="/signup" className='navigation-page'>Sign Up</Link>
-          
+
+      {/* Hamburger Button for Mobile */}
+      <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? <X size={24} /> : <Menu size={24} />}
+      </button>
+
+      {/* Nav Links */}
+      <div className={`navigation ${menuOpen ? 'show' : ''}`}>
+        <nav className="navigation-lists">
+          <Link to="/home" className="navigation-page">Home</Link>
+          <Link to="/contact" className="navigation-page">Contact</Link>
+          <Link to="/about" className="navigation-page">About</Link>
+          <Link to="/" className="navigation-page">Sign Up</Link>
         </nav>
       </div>
-      <div className='input_type'>
-        <div className='input-type-main'>
-          <input type="text" placeholder='what are you looking for?' className='input-type-all'/>
-          <SearchIcon/>
+
+      {/* Search + Icons */}
+      <div className="input_type">
+        <div className="input-type-main">
+          <input
+            type="text"
+            placeholder="What are you looking for?"
+            className="input-type-all"
+          />
+          <SearchIcon />
         </div>
-        <div className='icons'>
-        <FiHeart className='heart-icon' size={22}/>
-        <ShoppingCart className='shopping-cart' size={22}/>
-        </div>   
+        <div className="icons">
+          <FiHeart className="heart-icon" size={22} />
+          <ShoppingCart className="shopping-cart" size={22} />
+        </div>
       </div>
-         
     </div>
   )
 }
